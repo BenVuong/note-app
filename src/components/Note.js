cd5bd84c1abd81c038131d7522f337b08b3a2b58
Ben G Vuong <ben.vuong2000@gmail.com>
2023-07-17 11:27:06 -0500

    Added css styling to notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
-import{mdDeleteForver} from 'react-icons/md'
+import{MdDeleteForever} from 'react-icons/md'
 const Note = () => {
     return(
         <div className="note">
             <span>Hello World, to first note</span>
             <div className="note-footer">
                 <small>Note Footer goes here</small>
-                <mdDeleteForver className='delete-icon' size='1.3em'/>
+                <MdDeleteForever className='delete-icon'/>
             </div>
         </div>
     )
